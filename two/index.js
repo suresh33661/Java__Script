83be0b0002b1a36329f1e8290473539c6e761d1a
suresh33 <sureshpragada3366@gmail.com>
2023-11-27 15:01:38 +0530

    edited

---
+++ two/index.js
@@ -0,0 +1,16 @@
+"use strict"
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+console.log(btnsOpenModal);
+
+let a = [];
+for(let i =0 ; i<btnsOpenModal.length; i++){
+    btnsOpenModal[i].addEventListener("click", function(){
+        console.log("Button clicked");
+        modal.classList.remove("hidden");
+        overlay.classList.remove("hidden");
+    })
+}
