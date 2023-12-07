82e68e912911938dfd222910342511b01fc80550
suresh33 <sureshpragada3366@gmail.com>
2023-12-07 12:34:10 +0530

    changes

--- Three/index.js
+++ Three/index.js
@@ -1,3 +1,11 @@
-console.log(job);
+const game = {
+    
+        players: [ ['Smith', 'Warner', 'Maxwell', 'Starc',
+         'Cummins', 'Finch', 'Marsh', 'Zampa', 'Hazlewood', 'Stoinis', 'Wade'],
+        ['Kohli', 'Rohit', 'Dhawan', 'Pandya', 'Bumrah', 
+        'Rahul', 'Jadeja', 'Shami', 'Saini', 'Kuldeep', 'Saha']]
 
-const job = 'programmer';	
+
+};
+
+const [player1,player2] = game.players;
